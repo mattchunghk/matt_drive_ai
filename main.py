d1349658c0237de25d241ce5d91966b3802f6371
mattchunghk <mattchunghk@gmail.com>
2022-10-06 18:02:38 +0800

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import pygame
 import os
 import math
@@ -5,7 +6,7 @@ import sys
 import neat
 
 SCREEN_WIDTH = 2560
-SCREEN_HEIGHT = 1000
+SCREEN_HEIGHT = 1016
 glass_color = (2, 105, 31, 255)
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Drive AI")
@@ -20,31 +21,27 @@ class Car(pygame.sprite.Sprite):
             os.path.join("Assets", "car.png"))
         self.image = self.original_image
         self.rect = self.image.get_rect(center=(490, 820))
-        self.drive_state = False
+        # self.drive_state = False
         self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
         self.rotation_vel = 5
         self.direction = 0
         self.alive = True
+        self.radars = []
+        self.counter = 0
 
     def update(self):
+        self.radars.clear()
         self.drive()
         self.rotate()
         for radar_angle in (-60, -30, 0, 30, 60):
             self.radar(radar_angle)
         self.collision()
+        self.data()
 
     def drive(self):
-        if self.drive_state:
-            self.rect.center += self.vel_vector * 7
-            # if self.rect.left > SCREEN_WIDTH:
-            #     self.rect.right = 0
-            # if self.rect.right < 0:
-            #     self.rect.left = SCREEN_WIDTH
-            # if self.rect.top > SCREEN_HEIGHT:
-            #     self.rect.bottom = 0
-            # if self.rect.bottom < 0:
-            #     self.rect.top = SCREEN_HEIGHT
+        # if self.drive_state:
+        self.rect.center += self.vel_vector * 6
 
     def collision(self):
         length = 40
@@ -57,7 +54,6 @@ class Car(pygame.sprite.Sprite):
         if SCREEN.get_at(collision_point_right) == pygame.Color(2, 105, 31, 255) \
                 or SCREEN.get_at(collision_point_left) == pygame.Color(2, 105, 31, 255):
             self.alive = False
-            print("car is dead")
 
         # Draw Collision Points
         pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_right, 4)
@@ -92,11 +88,45 @@ class Car(pygame.sprite.Sprite):
                          self.rect.center, (x, y), 1)
         pygame.draw.circle(SCREEN, (0, 255, 0, 0), (x, y), 3)
 
+        dist = int(math.sqrt(
+            math.pow(self.rect.center[0] - x, 2)+math.pow(self.rect.center[1] - y, 2)))
 
-car = pygame.sprite.GroupSingle(Car())
+        self.radars.append([radar_angle, dist])
+
+    def data(self):
+        input = [0, 0, 0, 0, 0]
+        for i, radar in enumerate(self.radars):
+            input[i] = int(radar[1])
+        return input
+
+    def life(self):
+        self.counter += 1
+        if self.counter == 10000:
+            self.alive = False
 
 
-def eval_genomes():
+def remove(index):
+    cars.pop(index)
+    ge.pop(index)
+    nets.pop(index)
+
+
+# car = pygame.sprite.GroupSingle(Car())
+
+
+def eval_genomes(genomes, config):
+    global cars, ge, nets
+
+    cars = []
+    ge = []
+    nets = []
+
+    for genome_id, genome in genomes:
+        cars.append(pygame.sprite.GroupSingle(Car()))
+        ge.append(genome)
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        genome.fitness = 0
 
     run = True
     while run:
@@ -107,23 +137,63 @@ def eval_genomes():
 
         SCREEN.blit(TRACK, (0, 0))
 
-        user_input = pygame.key.get_pressed()
-        if sum(pygame.key.get_pressed()) <= 1:
-            car.sprite.drive_state = False
-            car.sprite.direction = 0
+        if len(cars) == 0:
+            break
 
-        if user_input[pygame.K_SPACE]:
-            car.sprite.drive_state = True
+        for i, car in enumerate(cars):
+            car.sprite.life()
+            ge[i].fitness += 1
+            if not car.sprite.alive:
+                remove(i)
 
-        if user_input[pygame.K_RIGHT]:
-            car.sprite.direction = 1
+        for i, car in enumerate(cars):
 
-        if user_input[pygame.K_LEFT]:
-            car.sprite.direction = -1
+            output = nets[i].activate(car.sprite.data())
+            # print(output)
 
-        car.draw(SCREEN)
-        car.update()
+            if output[0] > 0.7:
+                car.sprite.direction = 1
+            if output[1] > 0.7:
+                car.sprite.direction = -1
+            if output[0] <= 0.7 and output[1] <= 0.7:
+                car.sprite.direction = 0
+
+        # user_input = pygame.key.get_pressed()
+        # if sum(pygame.key.get_pressed()) <= 1:
+        #     car.sprite.drive_state = False
+        #     car.sprite.direction = 0
+
+        # if user_input[pygame.K_SPACE]:
+        #     car.sprite.drive_state = True
+
+        # if user_input[pygame.K_RIGHT]:
+        #     car.sprite.direction = 1
+
+        # if user_input[pygame.K_LEFT]:
+        #     car.sprite.direction = -1
+        for car in cars:
+            car.draw(SCREEN)
+            car.update()
+
         pygame.display.update()
 
 
-eval_genomes()
+# eval_genomes()
+
+def run(config_path):
+    global pop
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    pop = neat.Population(config)
+
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+
+    pop.run(eval_genomes, 50)
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
